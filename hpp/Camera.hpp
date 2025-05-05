9180f3b0e80714a594f87dd8421476bde265be20
Dein nigglaszahn <niclaszahn16@gmail.com>
2025-05-05 13:22:17 +0200

    Camera cpp hinzugefügt

--- hpp/Camera.hpp
+++ hpp/Camera.hpp
@@ -1,5 +1,5 @@
 #include "Ray.hpp"
-#include "Vector3D"
+#include "Vector3D.hpp"
 
 
 class Camera {
@@ -15,7 +15,16 @@ class Camera {
         int lengthPixels;
 
     public:
+
+        Camera();
         Vector3D get_pixel(int x, int y) const;
         Ray get_ray(int x, int y) const ;
 
+        Vector3D get_eye() const;
+        Vector3D get_view() const;
+        float get_width() const;
+        float get_height() const;
+        int get_width_pixels() const;
+        int get_length_pixels() const;
+        
 }
--- implementation/Camera.cpp
+++ implementation/Camera.cpp
@@ -1,3 +1,27 @@
 #include "../hpp/Camera.hpp"
 
 Camera::Camera(){}
+
+Vector3D Camera::get_eye() const {
+    return eye;
+}
+
+Vector3D Camera::get_view() const {
+    return view;
+}
+
+float Camera::get_width() const {
+    return width;
+}
+
+float Camera::get_height() const {
+    return height;
+}
+
+int Camera::get_width_pixels() const {
+    return widthPixels;
+}
+
+int Camera::get_length_pixels() const {
+    return lengthPixels;
+}
