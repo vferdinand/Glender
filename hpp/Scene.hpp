eb7279eee698206d05994de9e61c74ecdd8efcc2
Dein nigglaszahn <niclaszahn16@gmail.com>
2025-05-12 14:37:21 +0200

    nix neu

--- hpp/Scene.hpp
+++ hpp/Scene.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <vector>
 #include "Triangle.hpp"
+#include "Hitpoint.hpp"
 
 class Scene {
     private:
         std::vector<Triangle> triangles;
+        std::vector<Point3D> points;
     public:
         void addTriangle(Triangle tri);
 
