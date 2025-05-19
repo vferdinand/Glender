e08b1358507fba3071dcea710a20c0a80ed84e6e
Dein nigglaszahn <niclaszahn16@gmail.com>
2025-05-19 15:00:21 +0200

    Zwischenspeichern des 1D Arrays

--- hpp/Scene.hpp
+++ hpp/Scene.hpp
@@ -15,10 +15,10 @@ class Scene {
         const std::vector<Vertex> vertices;
         std::vector<RGBA> colors;
 
-        Image transformHitpointsToImage(std::vector<std::vector<Hitpoint>> hitpoints);
+        Image transformHitpointsToImage(std::vector<Hitpoint> hitpoints);
         Image transformHitpointsToImage(std::vector<Hitpoint> hitpoints, size_t height, size_t width);
         //Berechnung der Schnittpunkte von Rays und Triangles
-        std::vector<std::vector<Hitpoint>> calculateHitpoints(const std::vector<std::vector<Ray>>& rays);
+        std::vector<Hitpoint> calculateHitpoints(std::vector<Ray>& rays);
         //Farbe der Dreiecke Pixeln zuweisen
         const std::vector<Triangle>& getTriangles() const;
         
