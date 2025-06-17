8eb7e5c263476c7bf76b503435763ac6ed96c6ac
vferdinand <ferdinand.voelkel@web.de>
2025-06-17 13:47:33 +0200

    extract Spinners but not testet yet (+still in main just in case)

---
+++ hpp/Spinner.hpp
@@ -0,0 +1,11 @@
+#include "Scene.hpp"
+#include "Image.hpp"
+
+class Spinner {
+    public:
+        static void carSpinner();
+        static void lightSpinner();
+        static void carSpinnerFrames();
+        static void lightSpinnerFrames();
+        static void combinedSpinnerFrames();
+};
